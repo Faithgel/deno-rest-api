7776ef7df8210d4f8f1abbe14b8cfda0eed3c993
Angelo <racering14@gmail.com>
2022-07-05 22:51:34 -0400

    Changed Port ENV

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 
-const PORT = Deno.env.get("PORT") || "5000";
+const PORT = parseInt(Deno.env.get("PORT") || "5000");
 const app = new Application();
 
 app.use((ctx) => {
       ctx.response.body = "Hello World!";
 });
 
+console.log(`Listening on port ${PORT}`);
 await app.listen({port: +PORT});
