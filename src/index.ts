62305363feea74e9e582995404fca9056e9c8ff6
Angelo <racering14@gmail.com>
2022-07-06 21:28:31 -0400

    idk but this sh** would be work xDDD

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import application from deno oak module to use oak framework with lastest version of framework
 import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+const PORT = Deno.env.get("PORT") || "8080";
 
 const app = new Application();
 
@@ -7,4 +8,4 @@ app.use((ctx) => {
       ctx.response.body = "Hello World!";
 });
 console.log("Server is running on port http://localhost:8000");
-await app.listen({ port: 8000 });
+await app.listen({ port: +PORT });
