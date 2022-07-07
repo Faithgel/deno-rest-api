e5850178376bacd80eb30be23942da1e15659f0d
Angelo <racering14@gmail.com>
2022-07-06 22:27:49 -0400

    routes manager added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-// import application from deno oak module to use oak framework with lastest version of framework
+// import application from deno oak module
 import { Application } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+//import router from deno oak module
+import router from "./routes/index.routes.ts";
+
 const PORT = Deno.env.get("PORT") || "8080";
 
 const app = new Application();
 
-app.use((ctx) => {
-      ctx.response.body = "Hello World!";
-});
-console.log("Server is running on port http://localhost:8000");
+app.use(router.routes());
+console.log("Server is running on port http://localhost:" + PORT);
 await app.listen({ port: +PORT });
